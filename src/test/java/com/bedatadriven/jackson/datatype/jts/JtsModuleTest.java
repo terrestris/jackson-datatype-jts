29bdb3688ec024d2e3a3afe1991563d765810a47
Andre Henn <henn@terrestris.de>
2024-06-13 13:57:06 +0200

    chore: update jackson-databind to version 2.13.4.2 and jts to v 1.19.0

--- src/test/java/com/bedatadriven/jackson/datatype/jts/JtsModuleTest.java
+++ src/test/java/com/bedatadriven/jackson/datatype/jts/JtsModuleTest.java
@@ -1,17 +1,15 @@
 package com.bedatadriven.jackson.datatype.jts;
 
+
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-import org.locationtech.jts.geom.*;
 import org.easymock.EasyMock;
 import org.junit.Before;
 import org.junit.Test;
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.GeometryFactory;
 
 import java.io.IOException;
-
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
 
 public class JtsModuleTest {
 	private GeometryFactory gf = new GeometryFactory();
@@ -28,12 +26,12 @@ public class JtsModuleTest {
 		String json = "{\"type\":\"Singularity\",\"coordinates\":[]}";
 		mapper.readValue(json, Geometry.class);
 	}
-	
+
 	@Test(expected = JsonMappingException.class)
 	public void unsupportedGeometry() throws IOException {
 		Geometry unsupportedGeometry = EasyMock.createNiceMock("NonEuclideanGeometry", Geometry.class);
 		EasyMock.replay(unsupportedGeometry);
-		
+
 		mapper.writeValue(System.out, unsupportedGeometry);
 	}
 
